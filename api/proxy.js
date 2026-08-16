640d2ac116555aee3dfeaff11109dc35e892a33b
pixifyofficial52-pixel <pixifyofficial52@gmail.com>
2026-08-16 16:45:58 +0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 // ========== CONFIGURATION ==========
-const MAIN_APP_URL = 'https://live-whats-chatting-production.up.railway.app';
+const MAIN_APP_URL = 'https://live-whats-chatting-production.up.railway.app/';
 const ADMIN_API_KEY = 'hjchat-admin-secret-key-2024';
 
 module.exports = async (req, res) => {
